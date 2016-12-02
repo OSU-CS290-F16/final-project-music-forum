672331ae8009815895a6354e298c5795b81f16da
alexzdn <zadnipro@oregonstate.edu>
2016-12-02 15:43:29 -0800

    Fixing bugs for Posting functionality

--- public/index.js
+++ public/index.js
@@ -29,14 +29,14 @@ function closeAddPostModal() {
   backdropElem.classList.add('hidden');
   addPostModalElem.classList.add('hidden');
 
-  clearTodoInputValues();
+  clearPostInputValues();
 
 }
 
 /*
  * This function clears any value present in any of the todo input elements.
  */
-function clearTodoInputValues() {
+function clearPostInputValues() {
 
   var postInputElems = document.getElementsByClassName('post-input-element');
   for (var i = 0; i < postInputElems.length; i++) {
@@ -47,45 +47,60 @@ function clearTodoInputValues() {
 }
 
 /*
- * This function creates a single child for the todo-body element.  These
- * children contain the "where", "when", "who", and "details" information for
- * the todo.  They are formatted as a <p> element containing the provided
- * text.  If a label is provided, that label is added as a special <span>
- * element as the first child of the <p>, and the <p> is given class
- * "indent-wrapped".  The result is either this:
- *
- * <p>text<p>
- *
- * or
- *
- * <p class="indent-wrapped"><span class="label">label: </span>text</p>
+ * This function creates a single child for the post-body element.
  */
-function createNewPostBodyElem(text, label) {
+function createNewPostBodyElem(title, label, text) {
 
   var newPostBodyElem = document.createElement('p');
 
   // If the body element has a label, add a span containing that label.
   if (label) {
-    newPostBodyElem.classList.add('title-style');
-    var labelSpanElem = document.createElement('span');
-    labelSpanElem.classList.add(label);
-    var labelSpanElemText = document.createTextNode(label + ': ');
-    labelSpanElem.appendChild(labelSpanElemText);
-    newPostBodyElem.appendChild(labelSpanElem);
+	  
+	newPostBodyElem.classList.add('title-style');
+	var labelSpanElem = document.createElement('span');
+	labelSpanElem.classList.add(label);
+	var labelSpanElemText = document.createTextNode(title + ': ');
+	labelSpanElem.appendChild(labelSpanElemText);
+	newPostBodyElem.appendChild(labelSpanElem);
+	
   }
 
   var newPostBodyElemText = document.createTextNode(text);
   newPostBodyElem.appendChild(newPostBodyElemText);
 
-  return newTodoBodyElem;
+  return newPostBodyElem;
 
 }
 
+/*special case function for user name*/
+function createNewPostBodyUserName(title, label, text) {
+
+  var newPostBodyElem = document.createElement('p');
+
+  // If the body element has a label, add a span containing that label.
+  if (label) {
+	  
+	newPostBodyElem.classList.add('title-style');
+	var labelDivElem = document.createElement('div');
+	labelDivElem.classList.add(label);
+	var labelSpanElem = document.createElement('span');
+	labelSpanElem.classList.add('posted_by');
+	var labelSpanElemText = document.createTextNode(title + ' ');
+	labelSpanElem.appendChild(labelSpanElemText);
+	labelDivElem.appendChild(labelSpanElem);
+	newPostBodyElem.appendChild(labelDivElem);
+  }
+  
+  var newPostBodyElemText = document.createTextNode(text);
+  labelDivElem.appendChild(newPostBodyElemText);
+
+  return newPostBodyElem;
+}
 /*
  * This function creates a new <section> element for a post given all the
  * possible things that could go into the post.
  */
-function createNewPostSection(song-name, artist-name, description, user-name) {
+function createNewPostSection(song_name, artist_name, description, user_name) {
 
   // Create a new post section element.
   var newPostSection = document.createElement('section');
@@ -96,18 +111,18 @@ function createNewPostSection(song-name, artist-name, description, user-name) {
   newPostBody.classList.add('post-body');
 
 
-  var postBodySongName = createNewPostBodyElem(song-name, 'song-name');
+  var postBodySongName = createNewPostBodyElem('Song Name', 'song_name', song_name);
   newPostBody.appendChild(postBodySongName);
   
-  var postBodyArtistName = createNewPostBodyElem(artist-name, 'artist-name');
+  var postBodyArtistName = createNewPostBodyElem('Artist Name', 'artist_name', artist_name);
   newPostBody.appendChild(postBodyArtistName);
   
   if (description) {
-    var postBodyDescription = createNewPostBodyElem(description, 'description');
+    var postBodyDescription = createNewPostBodyElem('Description', 'description', description);
 	newPostBody.appendChild(postBodyDescription);
   }
   
-  var postBodyUserName = createNewPostBodyElem(user-name, 'user-name');
+  var postBodyUserName = createNewPostBodyUserName('Posted by', 'user_name', user_name);
   newPostBody.appendChild(postBodyUserName);
 
 
@@ -126,10 +141,10 @@ function createNewPostSection(song-name, artist-name, description, user-name) {
 function insertNewPost() {
 
   // Grab the values from all the input fields.
-  var postInputSongName = document.getElementById('todo-input-what').value;
-  var postInputArtistName = document.getElementById('todo-input-where').value;
-  var postInputDescription = document.getElementById('todo-input-when').value;
-  var postInputUserName = document.getElementById('todo-input-who').value;
+  var postInputSongName = document.getElementById('post-input-song-name').value;
+  var postInputArtistName = document.getElementById('post-input-artist').value;
+  var postInputDescription = document.getElementById('post-input-description').value;
+  var postInputUserName = document.getElementById('post-input-username').value;
 
 
   // We only add the note if we have a value for required fields
